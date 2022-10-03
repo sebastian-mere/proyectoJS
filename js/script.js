f99d7385b425e9c55058397396dc292d3a46148b
Sebastian Mere <sebastian.mere@gmail.com>
2022-10-03 20:29:49 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let nombre = prompt("Ingresa tu nombre");
+let edad = parseInt(prompt("Ingresa tu edad"));
+let aporte = parseInt(prompt("¿Cuánto podrías destinar a tu retiro por mes?"));
+let anios = parseInt(prompt("¿En cuántos años querrías retirarte?"));
+let provincia = prompt("Por último, ingresa la provincia de residencia").toLowerCase();
+
+let sellado = 0
+let edadRetiro = edad + anios;
+let meses = anios * 12;
+let fondo = 0;
+
+if (edad < 18) {
+
+    alert(nombre + " necesitas ser mayor de edad para poder continuar");
+
+} else {
+
+    for (let i = 1; i <= meses; i++) {
+
+        if (i <= 1) {
+            fondo = aporte
+            console.log(fondo)
+        } else {
+            fondo = (fondo + aporte) * 1.008
+            console.log(fondo)
+        }
+    }
+
+    switch (provincia) {
+        case "entre rios": sellado = aporte * 1.5;
+            break;
+    
+        case "buenos aires": sellado = aporte * 1.1;
+            break;
+    
+        case "santa fe": sellado = aporte * 2.5;
+            break;
+    
+        default: sellado = aporte * 1.2;
+    }
+
+    alert(nombre + " cuando tengas " + edadRetiro + " podrás retirarte con " + fondo.toFixed(2) +
+        "\nRecuerda que el débito de la primer cuota será de " + sellado.toFixed(2) + " debido a los costos de sellado de tu provincia");
+
+}
