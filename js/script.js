e47012b55eeb6da8256916f8bc615f6c00cc694c
Sebastian Mere <sebastian.mere@gmail.com>
2022-10-04 00:33:19 -0300

    funciones calcularFondos y costoSellado

--- js/script.js
+++ js/script.js
@@ -9,11 +9,7 @@ let edadRetiro = edad + anios;
 let meses = anios * 12;
 let fondo = 0;
 
-if (edad < 18) {
-
-    alert(nombre + " necesitas ser mayor de edad para poder continuar");
-
-} else {
+function calcularFondos() {
 
     for (let i = 1; i <= meses; i++) {
 
@@ -25,19 +21,34 @@ if (edad < 18) {
             console.log(fondo)
         }
     }
+}
+
+function costoSellado() {
 
     switch (provincia) {
         case "entre rios": sellado = aporte * 1.5;
             break;
-    
+
         case "buenos aires": sellado = aporte * 1.1;
             break;
-    
+
         case "santa fe": sellado = aporte * 2.5;
             break;
-    
+
         default: sellado = aporte * 1.2;
     }
+
+}
+
+if (edad < 18) {
+
+    alert(nombre + " necesitas ser mayor de edad para poder continuar");
+
+} else {
+
+    calcularFondos();
+
+    costoSellado();
 
     alert(nombre + " cuando tengas " + edadRetiro + " podrás retirarte con " + fondo.toFixed(2) +
         "\nRecuerda que el débito de la primer cuota será de " + sellado.toFixed(2) + " debido a los costos de sellado de tu provincia");
